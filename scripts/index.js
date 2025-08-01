7925bec0282d54ab40212b62861025200841f847
Ryan Welburn <ryanwelburn64@gmail.com>
2025-07-31 20:25:08 -0600

    New update

--- scripts/index.js
+++ scripts/index.js
@@ -4,25 +4,21 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileClosedBtn =
   editProfileModal.querySelector(".modal__close-btn");
 const newPostBtn = document.querySelector(".profile__new-post-btn");
-const newPostModal = document.querySelector(#new-post-modal);
+const newPostModal = document.querySelector("#new-post-modal");
 const newPostClosedBtn = newPostModal.querySelector(".modal__close-btn");
 
-  editProfileBtn.addEventListener("click", function () {
-editProfileModal.classList.add("modal_is-opened");
+editProfileBtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
+});
 
-  })
+editProfileClosedBtn.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+});
 
-  editProfileClosedBtn.addEventListener("click", function () {
-editProfileModal.classList.remove("modal_is-opened");
+newPostBtn.addEventListener("click", function () {
+  newPostModal.classList.add("modal_is-opened");
+});
 
-  })
-
-  newPostBtn.addEventListener("click", function () {
-newPostModal.classList.add("modal_is-opened");
-
-  })
-
-  newPostClosedBtn.addEventListener("click", function () {
-newPostModal.classList.remove("modal_is-opened");
-
-  })
+newPostClosedBtn.addEventListener("click", function () {
+  newPostModal.classList.remove("modal_is-opened");
+});
